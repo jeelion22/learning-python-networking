05db5a00070a984e6cde951591e86b75834ebe23
jeeva m <jeelion22@gmail.com>
2023-05-25 18:38:11 +0530

    adds comments for the use of sys module

--- RFC_download_urllib.py
+++ RFC_download_urllib.py
@@ -1,6 +1,9 @@
 import sys, urllib.request
 
 try:
+    # sys.argv[1] is used when the file is opened through
+    # terminal by providing the input as first argument
+    # python3 <file_name.py> <arg>
     rfc_number = int(sys.argv[1])
 
 except (IndexError, ValueError):
